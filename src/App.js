215852412bfc75e0b96fb08168623b3fd6a37357
Malikfasih <90088021+Malikfasih@users.noreply.github.com>
2022-03-15 16:08:13 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   useEffect(() => {
     const getMyNfts = async () => {
       const openseaData = await axios.get(
-        "https://testnets-api.opensea.io/assets?asset_contract_address=0xC36d211Da64a4cDD727F722196545Ec8799BeD9e&order_direction=asc", { mode: "cors" }
+        "https://testnets-api.opensea.io/assets?asset_contract_address=0xC36d211Da64a4cDD727F722196545Ec8799BeD9e&order_direction=asc"
       );
 //       console.log(openseaData.data.assets);
       setPunkListData(openseaData.data.assets);
